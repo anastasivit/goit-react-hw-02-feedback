ef23473daddcaa11cdc8ea241af0b570abcb2373
anastasivit <119945000+anastasivit@users.noreply.github.com>
2023-07-05 15:22:47 +0300

    Add fixed file App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,36 @@
 import React, { Component } from 'react';
-import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Notification from './Notification/Notification';
 import styles from './App.module.css';
 
 class App extends Component {
   state = {
-    feedback: {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    },
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
 
   handleFeedback = option => {
     this.setState(prevState => ({
-      feedback: {
-        ...prevState.feedback,
-        [option]: prevState.feedback[option] + 1,
-      },
+      [option]: prevState[option] + 1,
     }));
   };
 
   countTotalFeedback = () => {
-    const { feedback } = this.state;
-    const { good, neutral, bad } = feedback;
+    const { good, neutral, bad } = this.state;
     return good + neutral + bad;
   };
 
   countPositiveFeedbackPercentage = () => {
-    const { feedback } = this.state;
-    const { good } = feedback;
+    const { good } = this.state;
     const total = this.countTotalFeedback();
     return total === 0 ? 0 : Math.round((good / total) * 100);
   };
 
   render() {
-    const { feedback } = this.state;
+    const { good, neutral, bad } = this.state;
     const totalFeedback = this.countTotalFeedback();
     const positivePercentage = this.countPositiveFeedbackPercentage();
 
@@ -46,14 +39,16 @@ class App extends Component {
         <h1>My App</h1>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={Object.keys(feedback)}
+            options={Object.keys(this.state)}
             onLeaveFeedback={this.handleFeedback}
           />
         </Section>
         <Section title="Statistics">
           {totalFeedback > 0 ? (
             <Statistics
-              feedback={feedback}
+              good={good}
+              neutral={neutral}
+              bad={bad}
               total={totalFeedback}
               positivePercentage={positivePercentage}
             />
